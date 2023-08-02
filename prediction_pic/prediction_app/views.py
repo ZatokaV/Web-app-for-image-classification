beb94c335653ae876107457953e6083a0f4af689
Yaroslav <113246425+Invil124@users.noreply.github.com>
2023-08-02 22:40:15 +0300

    pred_f

--- prediction_pic/prediction_app/views.py
+++ prediction_pic/prediction_app/views.py
@@ -1,6 +1,10 @@
 import base64
 import copy
+import json
+import os
+import random
 
+from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
 from .vgg_classification import classify_image
@@ -12,12 +16,20 @@ def home(request):
         copy_image = copy.deepcopy(image)
 
         if image:
-            result = classify_image(image)
+            predicted_class, class_probabilities = classify_image(image)
             encoded_image = base64.b64encode(copy_image.read()).decode('utf-8')
 
+            json_path = os.path.join(settings.BASE_DIR, 'prediction_app', 'static', 'json', 'facts.json')
+            with open(json_path, "r") as json_file:
+                data = json.load(json_file)
+            facts_list = data[predicted_class]["facts"]
+            fact = random.choices(facts_list, k=1)[0]
+
             context = {
-                'predict_res': result,
+                'predict_res': predicted_class,
+                'probability': class_probabilities,
                 'image': encoded_image,
+                'fact': fact
             }
 
             return render(request, 'prediction_app/home.html', context=context)
